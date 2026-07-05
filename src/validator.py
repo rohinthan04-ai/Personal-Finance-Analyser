3b221012f4f5a04ba2420493b955a6dc37152bee
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-05 20:09:07 +0530

    Added Date Validation

--- src/validator.py
+++ src/validator.py
@@ -1,14 +1,30 @@
 import pandas as pd
+from datetime import datetime
+
 class Validator:
     def __init__(self,transactions):
         self.transactions = transactions
 
     def validate(self):
+        #cheacking the heading
         self.validate_field()
 
+        #cheacking the date
+        for date in self.transactions["Date"]:
+            if not self.validate_date(date):
+                print("Invalid date",date)
+                return
+        
     def validate_field(self):
         heading = set(self.transactions.columns.str.lower())
         if heading == {'amount', 'category', 'description', 'date', 'type'} :
             return
         else:
             print("The provided heading is invalid")
+
+    def validate_date(self,date):
+        try:
+            datetime.strptime(date,"%Y-%m-%d")
+            return True
+        except ValueError:
+            return False
