2828c108a6685d6d916a02e21c220cae0860f4a2
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-06 19:45:30 +0530

    Added Total income Analysis

--- src/analyser.py
+++ src/analyser.py
@@ -3,9 +3,14 @@ class Analyser:
     def __init__(self,df):
         self.df = df
     def analyse(self):
-        #Calculating the total expese
+        #Calculating the total expese,income,savings
         total_expense = self.calculate_total_expense(self.df[self.df["Type"].str.lower()=="expense"])
+        total_income = self.calculate_total_income(self.df[self.df["Type"].str.lower() == "income"])
+        print(total_income)
         print(total_expense)
 
     def calculate_total_expense(self,expense):
         return expense["Amount"].sum()
+    
+    def calculate_total_income(self,income):
+        return income["Amount"].sum()
