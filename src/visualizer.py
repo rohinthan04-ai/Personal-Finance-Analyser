09b4d81884e5adc6472eb58359a672cb64f2c5ee
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-09 20:18:21 +0530

    Added Category Expense - bar chart

--- src/visualizer.py
+++ src/visualizer.py
@@ -3,4 +3,13 @@ class Visualizer:
     def __init__(self,data):
         self.data = data
     def visualize(self):
-        print("Visualizer module check")
+        self.expense_bar(self.data["category_expense"])
+
+    def expense_bar(self,expense_data):
+        x = expense_data.keys()
+        y = expense_data.values()
+        plt.bar(x,y)
+        plt.title("Expense by Category")
+        plt.xlabel("Category")
+        plt.ylabel("Amount")
+        plt.show()
