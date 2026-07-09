74d6629124e422b584fb798db626b3c967de1b8f
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-09 21:10:51 +0530

    Added Savings insight

--- src/insights.py
+++ src/insights.py
@@ -3,5 +3,10 @@ class InsightGenerator:
         self.data = data
     def generate(self):
         insights=[]
-        
+        insights.append(self.savings())
+
         return insights
+        
+    def savings(self):
+        savings = self.data["total_savings"]
+        return "You have Saved ₹{}".format(savings)
