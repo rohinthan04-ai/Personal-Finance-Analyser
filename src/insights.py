ef0909459eca23e4c5d3dd2360ac3c89cc645b7b
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-12 11:28:11 +0530

    Finance summary insights are added

--- src/insights.py
+++ src/insights.py
@@ -3,10 +3,20 @@ class InsightGenerator:
         self.data = data
     def generate(self):
         insights=[]
-        insights.append(self.savings())
+        #finance summary
+        self.summary(insights)
 
+        
+        
         return insights
         
-    def savings(self):
+    def summary(self,insights):
         savings = self.data["total_savings"]
-        return "You have Saved ₹{}".format(savings)
+        income = self.data["total_income"]
+        expense = self.data["total_expense"]
+        average_expense = self.data["average_expense"]
+        insights.append(f"Your total income : ₹ {income}")
+        insights.append(f"Your total expense : ₹ {expense}")
+        insights.append(f"You have Saved : {savings}")
+        insights.append(f"Average expense per transaction :{average_expense}")
+   
