38909b3fbd60948e46e676c1c04f9e773f98fb7f
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-12 11:36:29 +0530

    Finance ratio insights are added

--- src/insights.py
+++ src/insights.py
@@ -6,7 +6,8 @@ class InsightGenerator:
         #finance summary
         self.summary(insights)
 
-        
+        #finance rations
+        self.ratios(insights)
         
         return insights
         
@@ -14,9 +15,17 @@ class InsightGenerator:
         savings = self.data["total_savings"]
         income = self.data["total_income"]
         expense = self.data["total_expense"]
-        average_expense = self.data["average_expense"]
+        average_expense = round(self.data["average_expense"],2)
         insights.append(f"Your total income : ₹ {income}")
         insights.append(f"Your total expense : ₹ {expense}")
         insights.append(f"You have Saved : {savings}")
         insights.append(f"Average expense per transaction :{average_expense}")
-   
+
+    def ratios(self,insights):
+        savings = self.data["total_savings"]
+        income = self.data["total_income"]
+        expense = self.data["total_expense"]
+        expense_ratio = round((expense/income)*100,2)
+        savings_ratio = round((savings/income)*100,2)
+        insights.append(f"You spend {expense_ratio}% of your income")
+        insights.append(f"You save {savings_ratio}% of your income")
