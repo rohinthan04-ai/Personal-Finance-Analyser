51c2216d1de01d2fe06c44a352c287b48f466794
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-12 13:25:46 +0530

    Finance health insights added

--- src/insights.py
+++ src/insights.py
@@ -9,8 +9,11 @@ class InsightGenerator:
         #finance rations
         self.ratios(insights)
         
-        
+        #category wise insights
         self.category_analysis(insights)
+        
+        #health insights
+        self.health(insights)
         return insights
         
        
@@ -48,3 +51,36 @@ class InsightGenerator:
         insights.append(f"{lowest_expense_category} is your least spending category")
         insights.append(f"{highest_income_category} is your Primary Source of income")
         insights.append(f"{lowest_income_category} is your smallest source of income.")
+
+    def health(self,insights):
+        savings = self.data["total_savings"]
+        income = self.data["total_income"]
+        expense = self.data["total_expense"]
+        savings_ratio = round((savings/income)*100,2)
+        expense_ratio = round((expense/income)*100,2)
+        if(expense<income):
+            insights.append("Your expense are within your income")
+        elif expense == income :
+            insights.append("⚠ Your income and expenses are equal. You are not saving any money.")
+        else :
+            insights.append("⚠ You are spending more than you earn.")
+
+        if savings_ratio > 30:
+            insights.append(f"Excellent savings rate ({savings_ratio}%)")
+        elif savings_ratio < 30 and savings_ratio > 10 :
+            insights.append(f"Your savings rate is healthy ({savings_ratio}%)")
+        else:
+            insights.append(f"⚠ Your savings rate is low.({savings_ratio}%)")
+
+        if expense_ratio < 70 :
+            insights.append("Your spending is well under control.")
+        elif expense_ratio > 70 and expense_ratio <90 :
+            insights.append("Most of your income goes toward expenses.")
+        else :
+            insights.append("⚠ Your expenses consume almost all of your income.")
+
+        if income > expense and savings_ratio >20 :
+            insights.append("Overall, your financial position looks healthy.")
+        elif expense >income :
+            insights.append("⚠ Your current financial position needs attention.")
+        
