856b55131b87b0852f696355109d12983b9c6214
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-13 19:18:33 +0530

    main() function polished

--- src/main.py
+++ src/main.py
@@ -3,21 +3,29 @@ from validator import Validator
 from analyser import Analyser
 from visualizer import Visualizer
 from insights import InsightGenerator
-reader = Reader("data/transactions.csv")
+def main():
+    #-----READING A FILE USING READER MODULE-----
+    reader = Reader("data/transactions.csv")
+    transactions = reader.read_csv()
 
-df = reader.read_csv()
+    #--------VALIDATING THE GIVEN FILE USING THE VALIDATOR MODULE-------
+    validator = Validator(transactions)
+    is_valid=validator.validate()
+    if not is_valid:
+       print("Validation fails, program terminated")
+       return
 
-vali = Validator(df)
-valid=vali.validate()
-if not valid:
-    print("Validation fails, program terminatd")
-    exit()
-anal = Analyser(df)
-analysed_results = anal.analyse()
+    #-----ANALYSING THE DATA USING THE ANALYSER MODULE----
+    analyser = Analyser(transactions)
+    analysed_results = analyser.analyse()
 
-visu = Visualizer(analysed_results)
-visu.visualize()
+    #-----VISUALIZING THE ANALYSED DATA USING THE VISUALIZER MODULE-----
+    visualyser = Visualizer(analysed_results)
+    visualyser.visualize()
 
-ingen = InsightGenerator(analysed_results)
-insights=ingen.generate()
-print(insights)
+    #-----GENERATING THE INSIGHTS FOR THE TRANSACTIONS FROM THE ANALYSED DATA-----
+    insight_generator = InsightGenerator(analysed_results)
+    insights=insight_generator.generate()
+
+if __name__ =="__main__":
+    main()
