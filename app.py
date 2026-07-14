8f12616dfd382ce020710998e2029a086c8a52bf
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-14 20:36:19 +0530

    Created the Welcome Desription

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import streamlit as st
+from src.read import Reader
+
 st.title("Personal Finance Analyser")
+st.markdown("""
+## Welcome!
+
+Upload your **transaction CSV** to begin.
+
+### Our app will:
+
+- Validate your data
+- Analyze income and expenses
+- Display charts
+- Generate financial insights
+""")
