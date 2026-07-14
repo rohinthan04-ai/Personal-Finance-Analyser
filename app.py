a5af3259082efe9e4bb5b25e3fc1819278eedbf8
rohinthan04-ai <rohinthan04@gmail.com>
2026-07-14 21:16:29 +0530

    Added Dashboard architrcture

--- app.py
+++ app.py
@@ -20,3 +20,27 @@ file = None
 file = st.sidebar.file_uploader("Upload your csv file",type=["csv"])
 if file != None:
     st.sidebar.write(f"Successfully uploaded the {file.name}")
+
+
+
+st.header("Validation")
+
+st.markdown("---")
+
+st.header("Finance Summary")
+col1,col2,col3 = st.columns(3)
+with col1:
+    st.subheader("Income")
+with col2:
+    st.subheader("Expense")
+with col3:
+    st.subheader("Savings")
+
+st.markdown("---")
+
+st.header("Charts")
+
+st.markdown("---")
+
+st.header("Finance Insights")
+
